9ce23c2c3da5b3ccddbe120c06bc2e4ae73e81ed
leiliang <liang.lei@mtime.com>
2017-07-12 13:22:20 +0800

    后台管理员登录提交

--- wine/src/com/chimu/wine/action/BaseAction.java
+++ wine/src/com/chimu/wine/action/BaseAction.java
@@ -3,7 +3,7 @@ package com.chimu.wine.action;
 import javax.servlet.http.HttpServletResponse;
 
 public abstract class BaseAction {
-    public void configResponse(HttpServletResponse response) throws Exception {
+    public void configResponse(HttpServletResponse response) {
 		response.setContentType("application/json");
 		response.addHeader("Access-Control-Allow-Origin", "*");
 	}
