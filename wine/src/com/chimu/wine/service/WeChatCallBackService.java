41af126f1940160f0ccbf70969e3d869988c437b
aiai5251 <934166463@qq.com>
2017-08-17 11:15:51 +0800

    接口不暴露

--- wine/src/com/chimu/wine/service/WeChatCallBackService.java
+++ wine/src/com/chimu/wine/service/WeChatCallBackService.java
@@ -119,4 +119,64 @@ public class WeChatCallBackService {
 //        return null;
 //    }
 
+
+//    ## message/custom/send: 发送消息
+//            ```
+//    {
+//        "touser": "",
+//            "msgtype": "news",
+//            "news": {
+//        "articles": [
+//        {
+//            "title": "好好",
+//                "description": "hhhh",
+//                "url": "jjj",
+//                "picurl": "aa"
+//        }
+//        ]
+//    }
+//    }
+//
+//```
+//
+//        ```
+//    {
+//        "touser": "",
+//            "template_id": "",
+//            "url": "https://worktile.com",
+//            "topcolor": "#000000",
+//            "data": {
+//        "first": {
+//            "value": "欢迎您关注东方酒业",
+//                    "color": "#000000"
+//        },
+//        "Day": {
+//            "value": "2017年8月9号 11:23:00",
+//                    "color": "#000000"
+//        },
+//        "orderId": {
+//            "value": "1500876221417",
+//                    "color": "#FF0033"
+//        },
+//        "orderType": {
+//            "value": "消费¥200",
+//                    "color": "#FF0033"
+//        },
+//        "customerName": {
+//            "value": "customerName",
+//                    "color": "#000000"
+//        },
+//        "customerPhone": {
+//            "value": "customerPhone",
+//                    "color": "#000000"
+//        },
+//        "remark": {
+//            "value": "快点发货哦！",
+//                    "color": "#000000"
+//        }
+//    }
+//    }
+//
+//```
+
 }
